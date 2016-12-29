d5366e01068f10c65589082a3185dfaaaca8158b
Yusuke Kurokawa <wotakuro@gmail.com>
2016-12-29 14:46:42 +0900

    update

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateAssetBundle {
+    [MenuItem("Tools/AssetBundleCreate")]
+    public static void Execute()
+    {
+        AssetDatabase.RemoveUnusedAssetBundleNames();
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.UncompressedAssetBundle,EditorUserBuildSettings.activeBuildTarget);
+    }
+}
--- Assets/TextureSwap/TextureSwap.cs
+++ Assets/TextureSwap/TextureSwap.cs
@@ -2,43 +2,128 @@
 using UnityEngine.UI;
 using System.IO;
 using System.Collections;
+using System.Runtime.InteropServices;
+
+
 namespace NoAssetBundlePrj
 {
     public class TextureSwap : MonoBehaviour
     {
-        public TextMesh result;
-        // Use this for initialization
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID )	&& ENABLE_IL2CPP
+    [DllImport("__Internal")]
+    private static extern void GC_expand_hp(int bytes);
+#endif
+
+        /// <summary>
+        /// 事前に ManagedHeapを確保しに行きます
+        /// </summary>
+        /// <param name="size">何バイト分確保したいか</param>
+        public static void AllocateGcMemory(int size)
+        {
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID ) && ENABLE_IL2CPP
+        GC_expand_hp(size);
+#endif
+        }
+
+        /// <summary>
+        /// シーンロード前に呼び出されます
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void AllocateGcBeforeStartApp()
+        {
+            AllocateGcMemory(1024 * 1024 * 40);
+        }
+
+        byte[] pvrBin;// = LoadDataFromStreamingAssets("test.data");
+        byte[] AbBin;// = LoadDataFromStreamingAssets("assetbundletest.ab");
+
         void Start()
         {
-            this.LoadTexture();
+            StartCoroutine(LoadBinData() );
         }
-        void LoadTexture()
+        IEnumerator LoadBinData()
         {
-            RawImage img = this.gameObject.GetComponentInChildren<RawImage>();
-            Texture2D tex = img.texture as Texture2D;
+            yield return null;
+            pvrBin = LoadDataFromStreamingAssets("test.data");
+
+            string path = Path.Combine(Application.streamingAssetsPath, "assetbundletest.ab");
+            WWW www = new WWW(path);
+            while (!www.isDone) { yield return null; }
+            //AbBin = www.bytes;
+
+            if (result != null)
+            {
+                result.text = "Ready";
+            }
+            AbBin = www.bytes;
+            Debug.Log("Complete Load");
+        }
+
+        public TextMesh result;
+        // Use this for initialization
+        public void TestExecute()
+        {
+            Debug.Log("KurokawaTest TestExecute");
+            int tryNum = 2;
+
+            float abStart = Time.realtimeSinceStartup;
+
+            Profiler.BeginSample("LoadFromAssetBundle");
+            for (int i = 0; i < tryNum; ++i)
+            {
+                this.LoadTextureFromAb(AbBin);
+            }
+            Profiler.EndSample();
+            float abEnd = Time.realtimeSinceStartup;
+
+            float rowStart = Time.realtimeSinceStartup;
+            Profiler.BeginSample("LoadFromRawData");
+            for (int i = 0; i < tryNum; ++i)
+            {
+                this.LoadTexture(pvrBin);
+            }
+            Profiler.EndSample();
+            float rowEnd = Time.realtimeSinceStartup;
+
+            result.text = "Ab : " + (abEnd - abStart) + "\nRaw:" + (rowEnd - rowStart);
+        }
+
+        void LoadTextureFromAb(byte[] bin)
+        {
 
             try
             {
-                string path = Path.Combine(Application.streamingAssetsPath, "test.data");
-#if UNITY_ANDROID && !UNITY_EDITOR
-
-            WWW www = new WWW(path);
-            while (!www.isDone) { }
-
-            byte[] bin = www.bytes;
-
-#else
-                byte[] bin = File.ReadAllBytes(path);
-#endif
-                Texture2D newTex = new Texture2D(2048,2048,TextureFormat.ETC2_RGB,false);
-                newTex.LoadRawTextureData(bin);
-                newTex.Apply();
-                img.texture = newTex;
-
+                if (bin == null)
+                {
+                    bin = LoadDataFromStreamingAssets("assetbundletest.ab");
+                }
+                AssetBundle ab = AssetBundle.LoadFromMemory(bin);
+                var all = ab.LoadAllAssets<Texture2D>();
+                ab.Unload(true);
+                ab = null;
+            }
+            catch (System.Exception e)
+            {
                 if (result != null)
                 {
+                    result.text = e.ToString();
                 }
-                Debug.Log(tex.name);
+            }
+        }
+
+        void LoadTexture(byte[] bin)
+        {
+
+            try
+            {
+                if (bin == null)
+                {
+                    bin = LoadDataFromStreamingAssets("test.data");
+                }
+                Texture2D newTex = new Texture2D(2048, 2048, TextureFormat.ETC2_RGB, false);
+                newTex.LoadRawTextureData(bin);
+                newTex.Apply();
+                Object.DestroyImmediate(newTex, false);
             }
             catch (System.Exception e)
             {
@@ -49,5 +134,23 @@ namespace NoAssetBundlePrj
 
             }
         }
+
+        byte[] LoadDataFromStreamingAssets(string file)
+        {
+
+            string path = Path.Combine(Application.streamingAssetsPath, file );
+#if UNITY_ANDROID && !UNITY_EDITOR
+
+            WWW www = new WWW(path);
+            while (!www.isDone) { }
+
+            byte[] bin = www.bytes;
+
+#else
+            byte[] bin = File.ReadAllBytes(path);
+#endif
+            return bin;
+        }
+
     }
 }
