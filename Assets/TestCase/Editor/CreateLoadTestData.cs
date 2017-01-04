9da5e71cfd91d937a7b682e0d028adaa84fa39bc
Yusuke Kurokawa <wotakuro@gmail.com>
2017-01-04 18:52:01 +0900

    update scene

--- Assets/TestCase/Editor/CreateLoadTestData.cs
+++ Assets/TestCase/Editor/CreateLoadTestData.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using System.IO;
 
 namespace NoAssetBundleTestPrj
 {
@@ -9,15 +10,27 @@ namespace NoAssetBundleTestPrj
         [MenuItem("Tools/LoadTest/CreateData")]
         public static void CreateTestData()
         {
+            TryCreateDirectory("Assets/StreamingAssets");
+            TryCreateDirectory("Assets/StreamingAssets/android");
+            TryCreateDirectory("Assets/StreamingAssets/ios");
             CreateAssetBundle();
             CreateRawTextureData();
+        }
+
+        private static void TryCreateDirectory(string dir)
+        {
+
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
         }
 
         public static void CreateAssetBundle()
         {
             AssetDatabase.RemoveUnusedAssetBundleNames();
 #if UNITY_ANDROID
-            BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/Android", BuildAssetBundleOptions.UncompressedAssetBundle, EditorUserBuildSettings.activeBuildTarget);
+            BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/android", BuildAssetBundleOptions.UncompressedAssetBundle, EditorUserBuildSettings.activeBuildTarget);
 #elif UNITY_IOS
         BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/ios", BuildAssetBundleOptions.UncompressedAssetBundle,EditorUserBuildSettings.activeBuildTarget);
 #else
@@ -31,11 +44,11 @@ namespace NoAssetBundleTestPrj
             Texture2D tex = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/test.png");
             byte[] binData = tex.GetRawTextureData();
 #if UNITY_ANDROID
-            System.IO.File.WriteAllBytes("Assets/StreamingAssets/android/test.data", binData);
+            File.WriteAllBytes("Assets/StreamingAssets/android/test.data", binData);
 #elif UNITY_IOS
-        System.IO.File.WriteAllBytes("Assets/StreamingAssets/ios/test.data", binData);
+            File.WriteAllBytes("Assets/StreamingAssets/ios/test.data", binData);
 #else 
-        EditorUtility.DisplayDialog("Only mobile", "Only mobile device support.Please switch to ios/android.", "OK");
+            EditorUtility.DisplayDialog("Only mobile", "Only mobile device support.Please switch to ios/android.", "OK");
 #endif
             RawTextureDataInfo info = new RawTextureDataInfo();
             info.width = tex.width;
@@ -43,9 +56,9 @@ namespace NoAssetBundleTestPrj
             info.formatType = (int)tex.format;
             info.mipmapFlag = (tex.mipmapCount > 1);
 #if UNITY_ANDROID
-            System.IO.File.WriteAllBytes("Assets/StreamingAssets/android/test.header", info.GetByteData() );
+            File.WriteAllBytes("Assets/StreamingAssets/android/test.header", info.GetByteData() );
 #elif UNITY_IOS
-            System.IO.File.WriteAllBytes("Assets/StreamingAssets/ios/test.header", info.GetByteData() );
+            File.WriteAllBytes("Assets/StreamingAssets/ios/test.header", info.GetByteData() );
 #endif
         }
 
